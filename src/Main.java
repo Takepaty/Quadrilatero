6f8ed0c074ff5a7daa2591c619b29eb6fbd4d61f
Patricia Takehara <paty.takeharab@outlook.com>
2022-06-03 14:12:07 -0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Quadrilátero
+        System.out.println("Exercício quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5,5);
+        Quadrilatero.area(7, 8, 9);
+        Quadrilatero.area(5f,5f);
     }
 }
--- src/Quadrilatero.java
+++ src/Quadrilatero.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Quadrilatero {
+public class Quadrilatero {
+
+    //Sobrecarga
+    public static void area(double lado){
+        System.out.println("Área do quafrado: " + lado * lado);
+    }
+
+    public static void area(double lado1, double lado2){
+        System.out.println("Área do retângulo: " + lado1 * lado2);
+    }
+
+    public static void area(double baseMaior, double baseMenor, double altura){
+        System.out.println("Área do trapézio: " + ((baseMaior + baseMenor) * altura)/ 2);
+    }
+
+    public static void area(float diagonal1, float diagonal2){
+        System.out.println("Área do losangulo: " + (diagonal1 * diagonal2)/2);
+    }
 }
